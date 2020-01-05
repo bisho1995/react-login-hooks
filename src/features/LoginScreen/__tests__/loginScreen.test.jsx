37d7f1089a09ac08f54bd4fd8b453339377635d2
bisvarup <bisvarup.mukherjee@tracxn.com>
2020-01-05 16:15:01 +0530

    updated login screen tests

--- src/features/LoginScreen/__tests__/loginScreen.test.jsx
+++ src/features/LoginScreen/__tests__/loginScreen.test.jsx
@@ -7,6 +7,13 @@ describe('<LoginScreen />', () => {
   it('should render', () => {
     const loginScreen = mount(<LoginScreen />);
     const loginScreenContainer = loginScreen.find('[data-test=\'login-screen\']');
+
     expect(loginScreenContainer.length).toBe(1);
+    expect(loginScreen.find("[data-test='healthify-header-info']").length).toBe(1);
+
+    /** Check after some time due to delay in lazy loading */
+    setTimeout(() => {
+      expect(loginScreen.find("[data-test='login-section']").length).toBe(1);
+    }, 500);
   });
 });
